e4d8d72a05f774f446b7a46a464e8d22d0220c55
Kaua-da-Cunha-Rodrigues <cunhakaua2@gmail.com>
2024-05-17 22:50:31 -0300

    Atualização do componente login e register
    Ajustes de registro feitas, a api não crasha mais. E login efetuado com sucesso, transmitindo o token de usuário para appointment.service.ts

--- Project_Medical_Appointment/src/app/modules/auth/components/login/login.component.ts
+++ Project_Medical_Appointment/src/app/modules/auth/components/login/login.component.ts
@@ -2,22 +2,24 @@ import { Component, OnInit } from '@angular/core';
 import {MatSelectModule} from '@angular/material/select';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { User } from '../../models/login-credentials';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { first } from 'rxjs';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { environment } from '../../../../../environments/environment';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule, MatSelectModule, MatInputModule, MatFormFieldModule],
+  imports: [ReactiveFormsModule, MatFormFieldModule, MatButtonModule, MatIconModule, MatToolbarModule, MatSelectModule, MatInputModule, RouterLink],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent implements OnInit{
-
-  //PARTE DE LOGIN AINDA NÃO ESTÁ EM FUNCIONAMENTO
 
   form!: FormGroup
 
@@ -29,15 +31,17 @@ export class LoginComponent implements OnInit{
 
   buildForm(): void{
     this.form = new FormGroup({
-      email: new FormControl(),
-      password: new FormControl()
+      email: new FormControl(null, [Validators.required, Validators.email]),  
+      password: new FormControl(null, [Validators.required, Validators.minLength(8)])
     })
   }
 
   login(): void{
     const user: User = this.form.getRawValue()
     this.authService.login(user).pipe(first()).subscribe({
-      complete: () =>{
+      next: (res) =>{
+        localStorage.setItem(environment.TOKEN_KEY, `Bearer ${res.token}`)
+        
         this.router.navigate(['appointment', 'list'])
       },
       error: (err) =>{
---
+++ Project_Medical_Appointment/src/app/modules/auth/models/user.model.ts
@@ -0,0 +1,6 @@
+export interface UserDatas{
+    name: string,
+    email: string,
+    password: string,
+    role: string
+}
