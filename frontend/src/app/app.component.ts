4f16783b214b880647efd8229a3a1aaf201a77c2
Kaua-da-Cunha-Rodrigues <cunhakaua2@gmail.com>
2024-05-22 21:38:44 -0300

    att

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,14 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./commons/header/header.component";
-import { FooterComponent } from "./commons/footer/footer.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, HeaderComponent, FooterComponent]
+    imports: [RouterOutlet, HeaderComponent]
 })
 export class AppComponent {
   title = 'Project_Medical_Appointment';
